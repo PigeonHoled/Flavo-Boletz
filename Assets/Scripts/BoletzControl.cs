213a5fefc70bd3db4f156a918aba6cdc15413bb1
DESKTOP-8FSGSN3\PigeonHoledNotebook <zbednyemaildozalozeniakontafb@gmail.com>
2018-01-27 05:46:18 +0100

    Add BoletzControl responsible for character's movement.

--- Assets/Scripts/BoletzControl.cs
+++ Assets/Scripts/BoletzControl.cs
@@ -8,17 +8,52 @@ public class BoletzControl : MonoBehaviour {
     GameObject Player;
 
     [SerializeField]
+    GameObject Camera;
+
+    [SerializeField]
+    Vector3 CameraPositionOffset;
+
+    [SerializeField]
+    Vector2 MouseSpeed;
+
+    [SerializeField]
     float Speed;
 
 	void Start () {
-		
-	}
+        Camera.transform.position = Player.transform.position + Camera.transform.localRotation * CameraPositionOffset;
+    }
 	
 	void Update () {
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+        float playerHorizontalMovement = Input.GetAxis("Horizontal");
+        float playerVerticalMovement = Input.GetAxis("Vertical");
 
-        Vector3 translation = Speed * Time.deltaTime * Vector3.Normalize(new Vector3(horizontalMovement, 0, verticalMovement));
-        Player.transform.position = Player.transform.position + translation;
+        // Applied so we don't normalize zero vector
+        if (Mathf.Approximately(playerHorizontalMovement, 0.0f) && Mathf.Approximately(playerVerticalMovement, 0.0f)) { return; }
+
+        Vector3 playerTranslation = Speed * Time.deltaTime * Vector3.Normalize(new Vector3(playerHorizontalMovement, 0, playerVerticalMovement));
+        Player.transform.position = Player.transform.position + Player.transform.localRotation * playerTranslation;
 	}
+
+    void LateUpdate() {
+        float cameraHorizontalAngle = Input.GetAxis("Mouse X") * Time.deltaTime * MouseSpeed.x;
+        float verticalAngle = -Input.GetAxis("Mouse Y") * Time.deltaTime * MouseSpeed.y;
+
+        Vector3 cameraRotation = Camera.transform.localRotation.eulerAngles;
+        float xAngle = cameraRotation.x + verticalAngle;
+        if (xAngle < 0) {
+            xAngle += 360;
+        }
+        else if (xAngle > 360) {
+            xAngle -= 360;
+        }
+
+        xAngle = (xAngle <= 179) ? Mathf.Clamp(xAngle, 0, 40) : Mathf.Clamp(xAngle, 320, 360);
+        cameraRotation = new Vector3(xAngle, cameraRotation.y + cameraHorizontalAngle, cameraRotation.z);
+        Camera.transform.localRotation = Quaternion.Euler(cameraRotation);
+
+        Quaternion playerRotation = Player.transform.localRotation;
+        Player.transform.localRotation = Quaternion.Euler(new Vector3(playerRotation.x, cameraRotation.y, playerRotation.z));
+
+        Camera.transform.position = Player.transform.position + Camera.transform.localRotation * CameraPositionOffset;
+    }
 }
