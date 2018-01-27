12c65535601471837ab43ce0bfde0bde2134ce4e
DESKTOP-8FSGSN3\PigeonHoledNotebook <zbednyemaildozalozeniakontafb@gmail.com>
2018-01-27 03:33:19 +0100

    Init commit

---
+++ Assets/Scripts/BoletzControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoletzControl : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Player;
+
+    [SerializeField]
+    float Speed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 translation = Speed * Time.deltaTime * Vector3.Normalize(new Vector3(horizontalMovement, 0, verticalMovement));
+        Player.transform.position = Player.transform.position + translation;
+	}
+}
