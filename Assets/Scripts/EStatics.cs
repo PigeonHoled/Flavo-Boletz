e5832da6e29003a3df29d462d0d45b777191ab59
DESKTOP-8FSGSN3\PigeonHoledNotebook <zbednyemaildozalozeniakontafb@gmail.com>
2018-01-27 15:40:01 +0100

    Add catching moles and coakroaches.

--- Assets/Scripts/EStatics.cs
+++ Assets/Scripts/EStatics.cs
@@ -5,6 +5,24 @@ using System.Text;
 
 namespace EStatics
 {
+    public enum EProjectile {
+        Mole = 0,
+        Coakroach,
+        None
+    };
+
+    public static class StringToEProjectile {
+        public static Dictionary<string, EProjectile> Get = new Dictionary<string, EProjectile>() {
+        { "Mole", EProjectile.Mole },
+        { "Coakroach", EProjectile.Coakroach }
+    };
+    }
+
+    public enum ETag {
+        Mole = 0,
+        Coakroach = 1,
+    }
+
     public enum ELayer {
         Projectile = 8,
         BankPart = 9
