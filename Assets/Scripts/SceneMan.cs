eef93df1eff2f9f94e6113167adc8accfa9dbd13
DESKTOP-8FSGSN3\PigeonHoledNotebook <zbednyemaildozalozeniakontafb@gmail.com>
2018-01-28 14:39:07 +0100

    Add StartScene

---
+++ Assets/Scripts/SceneMan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMan : MonoBehaviour {
+
+    public void LoadMainScene() {
+        Debug.Log("clicked");
+        Application.LoadLevel(1);
+    }
+
+    public void LoadIntroScene() {
+        Application.LoadLevel(0);
+    }
+}
