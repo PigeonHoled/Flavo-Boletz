12d049f20ee3dc2d0e32023518625352129c5831
DESKTOP-8FSGSN3\PigeonHoledNotebook <zbednyemaildozalozeniakontafb@gmail.com>
2018-01-28 14:17:51 +0100

    Add FlyingPartSpawner and sinusoide

--- Assets/Scripts/FlyingBankPart.cs
+++ Assets/Scripts/FlyingBankPart.cs
@@ -6,8 +6,25 @@ public class FlyingBankPart : MonoBehaviour {
 
     Rigidbody rb;
 
+    public float TimeConstant = 0.2f;
+    public float Amplitude = 3.0f;
+
     public void Awake() {
         rb = GetComponentInParent<Rigidbody>();
+    }
+
+    Vector3 initPosition;
+    private bool bWasHit = false;
+
+    private void Start() {
+        initPosition = transform.parent.position;
+        Debug.Log(transform.parent.position);
+    }
+
+    private void Update() {
+        if (bWasHit) { return; }
+
+        transform.parent.position = new Vector3(initPosition.x, initPosition.y + Mathf.Sin(Time.time * TimeConstant) * Amplitude, initPosition.z);
     }
 
     public void OnTriggerEnter(Collider other) {
@@ -16,5 +33,12 @@ public class FlyingBankPart : MonoBehaviour {
             rb.constraints = RigidbodyConstraints.FreezeRotation;
             Destroy(other.gameObject);
         }
+        bWasHit = true;
+    }
+
+    public void OnCollisionEnter(Collision collision) {
+        bWasHit = true;
+        rb.useGravity = true;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 }
---
+++ Assets/Scripts/FlyingPartSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingPartSpawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject PartPrefab;
+
+    [SerializeField]
+    List<Transform> Spawns;
+
+    [SerializeField]
+    List<GameObject> Parts;
+
+    [SerializeField]
+    Vector3 SpawnRotation;
+
+    private int lastID = 0;
+
+    [SerializeField]
+    float SpawnDelay;
+
+    private void Start() {
+        Parts = new List<GameObject>();
+        for (int i = 0; i < 4; i++)
+            Parts.Add(null);
+        Spawn();
+        StartSpawnCoroutine();
+    }
+
+    private void StartSpawnCoroutine() {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    IEnumerator SpawnCoroutine() {
+        yield return new WaitForSeconds(SpawnDelay);
+        Spawn();
+        StartSpawnCoroutine();
+    }
+
+    private void Spawn() {
+        int id = (lastID + 1) % 4;
+        lastID = id;
+        Vector3 position = Spawns[id].position;
+        if (Parts[id] != null)
+            Destroy(Parts[id]);
+
+        GameObject spawnedPart = Instantiate(PartPrefab, position, Quaternion.Euler(SpawnRotation));
+        Parts[id] = spawnedPart;
+    }
+
+}
